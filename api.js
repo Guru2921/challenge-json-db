2b87b4735b9eb5a511562ee10a83e21d33c343df
unknown <guru.softwaremaster@mail.ru>
2019-10-07 23:20:22 +0400

    module for rest api:put, get, delete

--- api.js
+++ api.js
@@ -2,11 +2,11 @@
 const fs = require('fs');
 
 module.exports = {
-  getHealth, putStudent, getStudent, deleteStudent
-}
-
-async function getHealth (req, res, next) {
-  res.json({ success: true })
+  putStudent,
+  getStudent, 
+  putStudentProperty, 
+  getStudentProperty, 
+  deleteStudentProperty
 }
 
 // CHECK IF IT'S JSON OR NOT
@@ -19,6 +19,7 @@ function isJSON(data) {
   }
   return ret;
 }
+
 //FILTER EMPTY ELEMENTS
 function filterEmpty(arr){
   var filteredArr = []
@@ -27,6 +28,7 @@ function filterEmpty(arr){
       filteredArr.push(arr[i])
   return filteredArr
 }
+
 // CHECK IF VALARIABLE IS STRING OR NOT
 function isTypeString(param)
 {
@@ -36,181 +38,251 @@ function isTypeString(param)
     return false
 }
 
-// PUT STUDENT PROPERTY
+// PUT STUDENT
 async function putStudent(req, res, next){
+  let studentId = req.params.studentId
+  let propertyContent = req.body;
+  let filePath = "data/"+studentId+".json";
+  try{
+    fs.exists(filePath, function(exists) {
+      //File exist
+      if(exists){
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if(err) {
+            res.send({status: 404, message: err})
+          }
+          else{
+            try{
+              let fileContent;
+              fileContent = JSON.parse(data);
+              let propertyItem = fileContent
+              for(var key in propertyContent){
+                propertyItem[key] = propertyContent[key]
+              }
+              let fileContentStr = JSON.stringify(fileContent)
+              fs.writeFile(filePath, fileContentStr, 'utf8', (err) => {
+                if(err) throw err
+                res.send({status: 200, message: "put student suceed"})
+              })
+            }catch(e) {console.log(e); res.send({status: 404, message: e})}
+          }
+        })
+      }
+      //File Not Exist
+      else{
+        try{
+          let fileContent = {}
+          for(var key in propertyContent){
+            fileContent[key] = propertyContent[key]
+          }
+          let fileContentStr = JSON.stringify(fileContent)
+          fs.writeFile(filePath, fileContentStr, (err) => {
+            if(err) throw err;
+            res.send({status: 200, message: "add student suceed"})
+          });
+        }catch(e) {
+          console.log(e);
+          res.send({status: 404, message: e})
+        }
+      }
+    })
+  } catch(e) {return res.send({status : 404, message: "file checking error"})}
+}
+
+// GET STUDENT
+async function getStudent (req, res, next) {
+  let studentId = req.params.studentId
+  let filePath = "data/" + studentId + ".json"
+  try{
+    fs.exists(filePath, function(exists) {
+      //File exist
+      if(exists){
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if(!err){
+            let fileContent;
+            fileContent = JSON.parse(data);
+            res.send(fileContent)
+          }
+        })
+      }
+      //File Not Exist
+      else{
+        res.send({'status' : 404, message: "file not exist"});
+      }
+    })
+  } catch(e) { res.send({status: 404, message: "file checking error"})}
+}
+
+// PUT STUDENT PROPERTY
+async function putStudentProperty(req, res, next){
   let studentId = req.params.studentId
   let properties = req.params[0].split('/')
   properties = filterEmpty(properties)
   let propertyContent = req.body;
   let filePath = "data/"+studentId+".json";
-  fs.exists(filePath, function(exists) {
-    //File exist
-    if(exists){
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if(err) {
-          res.send({status: 404, message: err})
-        }
-        else{
-          try{
-            let fileContent;
-            fileContent = JSON.parse(data);
-            let propertyItem = fileContent
-            for(var i=0; i<properties.length;i++){
-              var property = properties[i]
-              if(propertyItem.hasOwnProperty(property))
-              {
-                if(isTypeString(propertyItem[property]))
+  try{
+    fs.exists(filePath, function(exists) {
+      //File exist
+      if(exists){
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if(err) {
+            res.send({status: 404, message: err})
+          }
+          else{
+            try{
+              let fileContent;
+              fileContent = JSON.parse(data);
+              let propertyItem = fileContent
+              for(var i=0; i<properties.length;i++){
+                var property = properties[i]
+                if(propertyItem.hasOwnProperty(property))
                 {
+                  if(isTypeString(propertyItem[property]))
+                  {
+                    propertyItem[property] = {}
+                    propertyItem = propertyItem[property];
+                  }
+                  else
+                    propertyItem = propertyItem[property]                
+                }
+                else{
                   propertyItem[property] = {}
                   propertyItem = propertyItem[property];
                 }
-                else
-                  propertyItem = propertyItem[property]                
               }
-              else{
-                propertyItem[property] = {}
-                propertyItem = propertyItem[property];
+              for(var key in propertyContent){
+                propertyItem[key] = propertyContent[key]
               }
-            }
-            for(var key in propertyContent){
-              propertyItem[key] = propertyContent[key]
-            }
-            let fileContentStr = JSON.stringify(fileContent)
-            fs.writeFile(filePath, fileContentStr, 'utf8', (err) => {
-              if(err) throw err
-              console.log(fileContent + " written to " + filePath)
-              res.send(fileContent)
-            }) 
-          }catch(e) {console.log(e); res.send({status: 404, message: e})}
-        }
-      })
-    }
-    //File Not Exist
-    else{
-      try{
-        var fileContent = {}
-        var propertyItem = fileContent
-        for(var i=0; i<properties.length-1;i++){
-          var property = properties[i]
-          propertyItem[property] = {}
-          propertyItem = propertyItem[property];
-        }
-        for(var key in propertyContent){
-          let lastProperty = properties[properties.length-1]
-          if(lastProperty == ''){
+              let fileContentStr = JSON.stringify(fileContent)
+              fs.writeFile(filePath, fileContentStr, 'utf8', (err) => {
+                if(err) throw err
+                console.log(fileContent + " written to " + filePath)
+                res.send({status: 200, message: "put property succeed"})
+              }) 
+            }catch(e) {console.log(e); res.send({status: 404, message: e})}
+          }
+        })
+      }
+      //File Not Exist
+      else{
+        try{
+          var fileContent = {}
+          var propertyItem = fileContent
+          for(var i=0; i<properties.length;i++){
+            var property = properties[i]
+            propertyItem[property] = {}
+            propertyItem = propertyItem[property];
+          }
+          for(var key in propertyContent){
             propertyItem[key] = propertyContent[key]
           }
-          else{
-            if(!propertyItem.hasOwnProperty(lastProperty))
-              propertyItem[lastProperty] = {}
-            propertyItem[lastProperty][key] = propertyContent[key]
-          }
+          fileContent = JSON.stringify(fileContent)
+          fs.writeFile(filePath, fileContent, (err) => {
+            if(err) throw err;
+            console.log("written to" + filePath);
+            res.send({status: 200, message: "put property succeed"})
+          });
+        }catch(e) {
+          console.log(e);
+          res.send({status: 404, message: e})
         }
-        fileContent = JSON.stringify(fileContent)
-        fs.writeFile(filePath, fileContent, (err) => {
-          if(err) throw err;
-          console.log("written to" + filePath);
-          res.send(JSON.parse(fileContent))
-        });
-      }catch(e) {
-        console.log(e);
-        res.send({status: 404, message: e})
       }
-    }
-  })
+    })
+  } catch(e) {return {status : 404, message: "file checking error"}}
 }
 
 // GET STUDENT PROPERTY
-async function getStudent (req, res, next) {
+async function getStudentProperty (req, res, next) {
   let studentId = req.params.studentId
   let properties = req.params[0].split('/');
   properties = filterEmpty(properties)
   let filePath = "data/" + studentId + ".json"
-
-  fs.exists(filePath, function(exists) {
-    //File exist
-    if(exists){
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if(!err){
-          let fileContent;
-          fileContent = JSON.parse(data);
-          let propertyItem = fileContent;
-          let isExistProperty = true;
-          try{
-            properties.forEach(property => {
-              if(propertyItem.hasOwnProperty(property)){
-                propertyItem = propertyItem[property]
+  try{
+    fs.exists(filePath, function(exists) {
+      //File exist
+      if(exists){
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if(!err){
+            let fileContent;
+            fileContent = JSON.parse(data);
+            let propertyItem = fileContent;
+            let isExistProperty = true;
+            try{
+              properties.forEach(property => {
+                if(propertyItem.hasOwnProperty(property)){
+                  propertyItem = propertyItem[property]
+                } else{
+                  isExistProperty = false;
+                  throw breakException;
+                }
+              });
+              if(isExistProperty){
+                res.send(propertyItem)
               } else{
-                isExistProperty = false;
-                throw breakException;
+                res.send({status: 404, message: "property not exist"})
               }
-            });
-            if(isExistProperty){
-              res.send(propertyItem);
-            } else{
-              res.send({status: 404, message: "property not exist"})
-            }
-          } catch(e) { res.send({status: 404, message: "property not found"})} 
-        }
-      })
-    }
-    //File Not Exist
-    else{
-      res.send({'status' : 404, message: "file not exist"});
-    }
-  })
+            } catch(e) { res.send({status: 404, message: "property not found"})} 
+          }
+        })
+      }
+      //File Not Exist
+      else{
+        res.send({'status' : 404, message: "file not exist"});
+      }
+    })
+  } catch(e) { res.send({status: 404, message: "file checking error"})}
 }
 
 // DELETE STUDENT PROPERTY
-async function deleteStudent (req, res, next) {
+async function deleteStudentProperty (req, res, next) {
   let studentId = req.params.studentId
   let properties = req.params[0].split('/');
   properties = filterEmpty(properties)
   let filePath = "data/" + studentId + ".json"
-
-  fs.exists(filePath, function(exists) {
-    //File exist
-    if(exists){
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if(!err){
-          let fileContent;
-          fileContent = JSON.parse(data);
-          let propertyItem = fileContent;
-          let isExistProperty = true;
-          try{
-            for(var i=0; i<properties.length-1; i++){
-              var property = properties[i]
-              if(propertyItem.hasOwnProperty(property)){
-                propertyItem = propertyItem[property]
+  try{
+    fs.exists(filePath, function(exists) {
+      //File exist
+      if(exists){
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if(!err){
+            let fileContent;
+            fileContent = JSON.parse(data);
+            let propertyItem = fileContent;
+            let isExistProperty = true;
+            try{
+              for(var i=0; i<properties.length-1; i++){
+                var property = properties[i]
+                if(propertyItem.hasOwnProperty(property)){
+                  propertyItem = propertyItem[property]
+                } else{
+                  isExistProperty = false;
+                  throw breakException;
+                }
+              }
+              if(isExistProperty){
+                var lastProperty = properties[properties.length-1]
+                if(propertyItem.hasOwnProperty(lastProperty)){
+                  delete propertyItem[lastProperty]
+                  fileContent = JSON.stringify(fileContent)
+                  fs.writeFile(filePath, fileContent, (err) => {
+                    if(err) throw err;
+                    res.send({status: 200, message: "property removed"})
+                  });
+                }
+                else{
+                  res.send({status: 404, message: "property not exist"})
+                }
               } else{
-                isExistProperty = false;
-                throw breakException;
-              }
-            }
-            if(isExistProperty){
-              var lastProperty = properties[properties.length-1]
-              if(propertyItem.hasOwnProperty(lastProperty)){
-                delete propertyItem[lastProperty]
-                fileContent = JSON.stringify(fileContent)
-                fs.writeFile(filePath, fileContent, (err) => {
-                  if(err) throw err;
-                  console.log("written to" + filePath);
-                  res.send({status: 200, message: "property removed"})
-                });
-              }
-              else{
                 res.send({status: 404, message: "property not exist"})
               }
-            } else{
-              res.send({status: 404, message: "property not exist"})
-            }
-          } catch(e) { res.send({status: 404, message: "property not found"})} 
-        }
-      })
-    }
-    //File Not Exist
-    else{
-      res.send({'status' : 404, message: "file not exist"});
-    }
-  })
+            } catch(e) { res.send({status: 404, message: "property not found"})} 
+          }
+        })
+      }
+      //File Not Exist
+      else{
+        res.send({'status' : 404, message: "file not exist"});
+      }
+    })
+  }catch(e) {res.send({status: 404, message: "file checking eror"})}
 }
